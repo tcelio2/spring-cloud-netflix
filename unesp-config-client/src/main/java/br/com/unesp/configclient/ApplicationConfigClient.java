c9b0a849ef9b8c73b019f39ff2e1f8d38d7da4a1
Thiago Pereira Célio <thiago.pereira@adtsys.com.br>
2018-07-15 23:02:31 -0300

    adicionado projetos relacionado a microservico netflix

---
+++ unesp-config-client/src/main/java/br/com/unesp/configclient/ApplicationConfigClient.java
@@ -0,0 +1,15 @@
+package br.com.unesp.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApplicationConfigClient {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationConfigClient.class, args);
+    
+  }
+
+}
---
+++ unesp-config-client/src/main/java/br/com/unesp/configclient/controller/CommunicationClientController.java
@@ -0,0 +1,18 @@
+package br.com.unesp.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CommunicationClientController {
+
+  @Value("${lucky-word}") 
+  String luckyWord;
+
+  @GetMapping("/lucky-word")
+  public String showLuckyWord() {
+    return "The lucky word is: " + luckyWord;
+  }
+  
+}
